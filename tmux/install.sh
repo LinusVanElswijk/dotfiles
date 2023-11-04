1b005479ef33b9d1ea0b2c0cd9e2609bd0e8b201
Linus van Elswijk <linusvanelswijk@gmail.com>
2023-11-04 03:42:09 +0100

    tmux: Added tmux plugin manager.

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Install TPM.
+tpm_dest="${TMUX_PLUGIN_MANAGER_PATH}/tpm"
+mkdir -p $tpm_dest
+rm -rf $tpm_dest
+git clone https://github.com/tmux-plugins/tpm \
+  $tpm_dest \
+  --branch v3.1.0 \
+  --single-branch \
+  -c advice.detachedHead=false
+
+# Install the config file. 
 conf="$HOME/.tmux.conf"
 target=$(readlink -f "${CONFIG_SYNC_DIR}/tmux/config")
 echo "Replacing '$conf' with a link to '$target'."
