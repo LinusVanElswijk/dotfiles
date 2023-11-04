3685af814608f1dabc6cd9d66c0ef14e345fd2df
Linus van Elswijk <linusvanelswijk@gmail.com>
2023-11-04 02:32:30 +0100

    bash: Added config_sync function and CONFIG_SYNC_DIR env var.

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conf="$HOME/.tmux.conf"
-target=$(readlink -f "${BASH_SOURCE%/*}/config")
+target=$(readlink -f "${CONFIG_SYNC_DIR}/tmux/config")
 echo "Replacing '$conf' with a link to '$target'."
 rm -f $conf
 ln -s $target $conf 
--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conf="$HOME/.vimrc"
-target=$(readlink -f "${BASH_SOURCE%/*}/vimrc")
+target=$(readlink -f "${CONFIG_SYNC_DIR}/vim/vimrc")
 echo "Replacing '$conf' with a link to '$target'."
 rm -f $conf
 ln -s $target $conf 
