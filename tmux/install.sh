478df2252f0eb98ca2a5b4cc8598d58e9b4bc04e
Linus van Elswijk <linusvanelswijk@gmail.com>
2023-11-05 01:12:21 +0100

    tmux: Added gitmux integration.

--- tmux/install.sh
+++ tmux/install.sh
@@ -10,6 +10,27 @@ git clone https://github.com/tmux-plugins/tpm \
   --single-branch \
   -c advice.detachedHead=false
 
+# Install gitmux
+
+if command -v gitmux &> /dev/null; then
+  echo "gitmux is already installed."
+else
+  # Define the URL for gitmux download
+  GITMUX_URL="https://github.com/arl/gitmux/releases/download/v0.10.3/gitmux_v0.10.3_linux_amd64.tar.gz"
+  GITMUX_TMP_DIR="/tmp/gitmux_install_$(date +%s)"
+  
+  mkdir -p "$GITMUX_TMP_DIR"  
+  ( cd "$GITMUX_TMP_DIR" 
+    wget "$GITMUX_URL" -O gitmux.tar.gz
+    tar -xzf gitmux.tar.gz
+    chmod +x gitmux
+    mv gitmux ~/bin/
+  )
+  rm -rf "$GITMUX_TMP_DIR"
+  
+  echo "gitmux has been installed in ~/bin."
+fi
+
 # Install the config file. 
 conf="$HOME/.tmux.conf"
 target=$(readlink -f "${CONFIG_SYNC_DIR}/tmux/config")
