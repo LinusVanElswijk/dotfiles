6b66265a9d9219637284f4d353f1a13924868fde
Linus van Elswijk <linusvanelswijk@thirona.eu>
2023-11-09 18:07:55 +0100

    nvim: added nvim-airline with tommorow theme.

--- nvim/_config/nvim/lua/linus/lazy.lua
+++ nvim/_config/nvim/lua/linus/lazy.lua
@@ -17,6 +17,12 @@ require("lazy").setup({
     config = function()
       vim.cmd.colorscheme "catppuccin"
     end
+  },
+  { "vim-airline/vim-airline", name = "vim-airline" },
+  { "vim-airline/vim-airline-themes", name = "vim-airline-themes",
+    config = function()
+      vim.g.airline_theme="tomorrow"
+    end  
   }
 })
 
