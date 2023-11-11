0d595ef9cf98f8c156de8d91b348ab0bbf63694a
Linus van Elswijk <linusvanelswijk@gmail.com>
2023-11-12 00:56:46 +0100

    nvim: Switch from vim-airline to lightline

--- nvim/_config/nvim/lua/linus/plugins/vim-airline.lua
+++
@@ -1,8 +0,0 @@
-return({
-  { "vim-airline/vim-airline", name = "vim-airline" },
-  { "vim-airline/vim-airline-themes", name = "vim-airline-themes",
-    config = function()
-      vim.g.airline_theme="tomorrow"
-    end
-  }
-})
