9123925ecf084c41759828056a49a1126dc1ab5a
Linus van Elswijk <linusvanelswijk@thirona.eu>
2023-11-13 11:47:14 +0100

    nvim: Show git file status in lightline.

--- nvim/_config/nvim/lua/linus/plugins/lightline.lua
+++ nvim/_config/nvim/lua/linus/plugins/lightline.lua
@@ -1,7 +1,30 @@
 return(
   { "itchyny/lightline.vim", name = "lightline",
+    dependencies = { "airblade/vim-gitgutter" },
     config = function()
-      vim.g.lightline = { colorscheme = "catppuccin" }
+      vim.g.lightline = { 
+        colorscheme = "catppuccin",
+        active = {
+          left =  {
+            { 'mode', 'paste' },
+            { 'readonly', 'filename', 'git_status', 'modified' }
+          }
+        },
+        component_function =  {
+          git_status = 'LightlineGitStatus'
+        }
+      }
+
+      -- Define a vim function for the git_status component.
+      vim.cmd([[
+        function! LightlineGitStatus()
+          let [a,m,r] = GitGutterGetHunkSummary()
+          if (a + m + r) == 0
+            return ""
+          endif
+          return printf('+%d ~%d -%d ', a, m, r)
+        endfunction
+      ]])
     end
   }
 )
