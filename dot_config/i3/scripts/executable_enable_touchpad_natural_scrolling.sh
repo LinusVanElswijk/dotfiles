a7dc1c74f045a2a20a51a05fc53f3e439884047b
Linus van Elswijk <linusvanelswijk@gmail.com>
2025-01-20 00:18:43 +0100

    i3: Generalize enabling of natural scrolling

--- dot_config/i3/scripts/executable_enable_touchpad_natural_scrolling.sh
+++ dot_config/i3/scripts/executable_enable_touchpad_natural_scrolling.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touchpad_id=$(xinput list | grep Touchpad | cut -f2 -d= | cut -f1)
-prop_id_natural_scrolling=326
+prop_id_natural_scrolling=$(xinput list-props "${touchpad_id}" | grep "Natural Scrolling Enabled (" | cut -d\( -f2 | cut -d\) -f1)
 xinput set-prop "$touchpad_id" "$prop_id_natural_scrolling" 1
