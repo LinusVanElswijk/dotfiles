ccd0926c51ee560dd406951a658bff66a253d8c2
Linus van Elswijk <l.vanelswijk@science.ru.nl>
2025-02-22 16:44:09 +0100

    yazi: update starship config

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,11 +1,7 @@
-require("full-border"):setup({
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-})
 require("relative-motions"):setup({
 	show_numbers = "relative_absolute",
 	show_motion = false,
 	only_motions = true,
 	enter_mode = "first",
 })
-require("starship"):setup()
+require("starship"):setup({ config_file = "~/.config/yazi/starship.toml" })
