f7ee78ec62cee92ff6590383d40515edb3ddf067
Linus van Elswijk <linusvanelswijk@gmail.com>
2025-02-22 18:10:05 +0100

    fish: update config
    - start 'keychain' in interactive shells
    - remove fish_variables from chezmoi
      these variables will differ between machines

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if type -q keychain
+        set -l tmp $SHELL
+        set SHELL (which  fish)
+        keychain --eval --quiet -Q | source
+        set SHELL $tmp
+    end
 end
 
 if type -q starship
