d6a313a57585aebe2fd84f2c9d27910650e349f1
Linus van Elswijk <LinusVanElswijk@users.noreply.github.com>
2026-02-25 17:26:42 +0100

    fish: update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,10 +3,22 @@ if status is-interactive
         set -l tmp $SHELL
         set SHELL (which  fish)
         keychain --eval --quiet -Q | source
+        trap 'echo "SIGINT -> keychain unlock skipped"' SIGINT
+        keychain id_ed25519 --quiet -Q
+        trap - SIGINT
         set SHELL $tmp
     end
 end
 
 if type -q starship
     starship init fish | source
+end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
