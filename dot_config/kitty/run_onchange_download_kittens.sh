4284fcfdf0885004caa3693c0906ea413f5daf31
Linus van Elswijk <linusvanelswijk@pm.me>
2026-03-26 14:50:42 +0100

    Update .config/kitty/download_kittens.sh

--- dot_config/kitty/run_onchange_download_kittens.sh
+++ dot_config/kitty/run_onchange_download_kittens.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget --output-document='~/.config/kitty/jumptab.py'\
-  https://raw.githubusercontent.com/LinusVanElswijk/kitty-jumptab/refs/heads/main/src/jumptab.py
+wget -O ~/.config/kitty/jumptab.py https://raw.githubusercontent.com/LinusVanElswijk/kitty-jumptab/refs/heads/main/src/jumptab.py
