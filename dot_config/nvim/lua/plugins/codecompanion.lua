5a085efc2f639bd0c8b2b624f1c5474d48a4f4ec
Linus van Elswijk <linusvanelswijk@pm.me>
2026-04-09 13:55:08 +0200

    Update .config/nvim/lua/plugins/codecompanion.lua

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -3,59 +3,16 @@ return {
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
   opts = {
-    system_prompt = function(ctx)
-      return ctx.default_system_prompt .. "\
-        Write like you're chatting with a friend—relaxed and informal.\
-        Answer straight away; do not lead in answers.\
-        Do not add follow up suggestions."
-    end,
-    adapters = {
-      http = {
-        ollama = function()
-          return require("codecompanion.adapters").extend("ollama", {
-            env = {
-              url = "http://localhost:12434",
-            },
-            headers = {
-              ["Content-Type"] = "application/json",
-            },
-            parameters = {
-              sync = true,
-            },
-          })
-        end,
-      },
-    },
     interactions = {
-      chat = {
-        adapter = {
-          name = "ollama",
-          model = "docker.io/ai/ministral3:3B-Q4_K_M",
-        },
-      },
-      inline = {
-        adapter = {
-          name = "ollama",
-          model = "docker.io/ai/ministral3:3B-Q4_K_M",
-        },
-      },
-      cmd = {
-        adapter = {
-          name = "ollama",
-          model = "docker.io/ai/ministral3:3B-Q4_K_M",
-        },
-      },
-      background = {
-        adapter = {
-          name = "ollama",
-          model = "docker.io/ai/ministral3:3B-Q4_K_M",
-        },
-      },
+      chat = { adapter = { name = "opencode" }, },
+      inline = { adapter = { name = "opencode" } },
+      cmd = { adapter = { name = "opencode" } },
+      background = { adapter = { name = "opencode" } },
     },
   },
   keys = {
-    { "<leader>at", mode = { "n", "v" }, ":CodeCompanionChat Toggle<cr>", { desc = "Code Companion Chat Toggle"}},
-    { "<leader>ac", mode = { "n", "v" }, ":CodeCompanionChat<cr>", { desc = "Code Companion Chat"}},
-    { "<leader>aa", mode = { "n", "v" }, ":CodeCompanionActions<cr>", { desc = "Code Companion Actions" }},
-  },
+    { "<C-a>", mode = { "n", "v" }, ":CodeCompanionActions<cr>", { desc = "Code Companion Actions" } },
+    { "<leader>a", mode = { "n", "v" }, ":CodeCompanionChat Toggle<cr>", { desc = "Code Companion Chat Toggle" } },
+    { "ga", mode = "v", ":CodeCompanionChat Add<cr>", { desc = "Code Companion Chat Add" } },
+  }
 }
