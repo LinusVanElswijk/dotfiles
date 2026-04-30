a73c709887060618abd6bb246382cf82deef59c9
Linus van Elswijk <linusvanelswijk@pm.me>
2026-04-30 16:45:44 +0200

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-function P(obj)
-  print(vim.inspect(obj))
-end
+
 
 -- winbar
 vim.opt.winbar = "%=%f%y%m"
